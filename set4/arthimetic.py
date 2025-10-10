645c4b1107209295a8fa6ea03eb8a7ba9dbed4f9
Syamprasad1212 <syamofficial1212@gmail.com>
2025-10-10 15:22:04 +0530

    Create arthimetic.py

---
+++ set4/arthimetic.py
@@ -0,0 +1,10 @@
+a = 15
+b = 4
+
+print("a + b =", a + b)  # Addition
+print("a - b =", a - b)  # Subtraction
+print("a * b =", a * b)  # Multiplication
+print("a / b =", a / b)  # Division (float)
+print("a // b =", a // b) # Floor division (integer division)
+print("a % b =", a % b)  # Modulus (remainder)
+print("a ** b =", a ** b) # Exponentiation (a to the power b)
