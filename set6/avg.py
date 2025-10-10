be2d39a19a6cf7d8d1f496c03d4187f87f3eea33
Syamprasad1212 <syamofficial1212@gmail.com>
2025-10-10 15:29:56 +0530

    Create avg.py

---
+++ set6/avg.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40, 50]
+
+total_sum = sum(numbers)
+average = total_sum / len(numbers)
+
+print(f"Sum of list elements: {total_sum}")
+print(f"Average of list elements: {average}")
