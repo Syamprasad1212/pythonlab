24e111b6d280c31a271c2536254f443fa7539d5a
Syamprasad1212 <syamofficial1212@gmail.com>
2025-10-10 14:42:45 +0530

    Add files via upload

---
+++ multi.py
@@ -0,0 +1,16 @@
+class anu():
+	def talk(self):
+		print("why do u come this fast  ")
+
+class ven(anu):
+	def walk(self):
+		print("ntg work is done ")
+	
+class syam(ven):
+	def bark(self):
+		print("plz keep quite ")
+
+obj=syam()
+obj.talk()
+obj.bark()
+obj.walk()
