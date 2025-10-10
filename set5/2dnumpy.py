f832b7f4ea65694e3ce0927806e704669a0b03ac
Syamprasad1212 <syamofficial1212@gmail.com>
2025-10-10 15:26:45 +0530

    Create 2dnumpy.py

---
+++ set5/2dnumpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create a 2D NumPy array
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("Array:\n", arr)
+print("Number of dimensions (ndim):", arr.ndim)
+print("Shape of array:", arr.shape)
+print("Total number of elements (size):", arr.size)
+print("Data type of elements (dtype):", arr.dtype)
